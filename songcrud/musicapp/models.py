a4de3828f957955c621835be2fa7682f8ab5f560
WadzanayiK <67104735+WadzanayiK@users.noreply.github.com>
2022-11-01 10:10:28 +0200

    My second Commit

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -9,19 +9,23 @@ class Artiste(models.Model):
     age = models.IntegerField()
 
     def __str__(self):
-        return "%s %s" % (self.first_name, self.last_name)
+        return f"{self.first_name}{self.last_name}"
     
 class Song(models.Model):  
     artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
     release_date = models.DateField()
     likes = models.IntegerField()
     title =models.CharField(max_length =50)
+    
+    def __str__(self):
+        return f"{self.title}"
         
 class Lyric(models.Model):
     song_id = models.ForeignKey(Song, on_delete=models.CASCADE)    
     content = models.CharField(max_length =50000)
     
-                
-    
-        
-        
+    def __str__(self):
+        if len(self.content) > 50:
+            return f"{self.content[0:50]}..."
+        else:
+            return f"{self.content}"
